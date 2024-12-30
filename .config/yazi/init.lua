12a183515f468abd942d3a0066ab087d08dd6a55
luccaugusto <lucca@luccaaugusto.xyz>
2024-12-30 18:48:56 -0300

    fix yazi deprecated configs

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,6 +1,6 @@
 function Linemode:size_and_mtime()
 	local year = os.date("%Y")
-	local time = math.floor(self._file.cha.modified or 0)
+	local time = math.floor(self._file.cha.mtime or 0)
 
 	if time > 0 and os.date("%Y", time) == year then
 		time = os.date("%b %d %H:%M", time)
